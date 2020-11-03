9b8041d55eba7257e3d46b2e0df6045f48088380
thelastblade <54807143+thelastblade@users.noreply.github.com>
2020-11-03 13:34:30 +0800

    commit

---
+++ app/src/main/java/com/example/cheq/RestaurantActivity.java
@@ -0,0 +1,16 @@
+package com.example.cheq;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.testapp.R;
+
+public class RestaurantActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restaurant_home);
+    }
+}
