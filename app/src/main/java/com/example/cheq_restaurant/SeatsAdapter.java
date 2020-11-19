c1607aeb0244a892ba79911f2fe3c2ee2cf0dae9
thelastblade <54807143+thelastblade@users.noreply.github.com>
2020-11-19 17:13:21 +0800

    Revert "commit"
    This reverts commit 327a8e36804a80df07c5b33197df4674ebed58c7.

---
+++ app/src/main/java/com/example/cheq_restaurant/SeatsAdapter.java
@@ -0,0 +1,92 @@
+package com.example.cheq_restaurant;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Parcelable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SeatsAdapter extends RecyclerView.Adapter<SeatsAdapter.ViewHolder> {
+    private ArrayList<Seat> seats;
+
+    public SeatsAdapter(ArrayList<Seat> seats){this.seats = seats;}
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView restaurantAllQueuePax,restaurantAllQueueSeatNo;
+        Button restaurantAllQueueSeatButton,restaurantAllQueueCancelButton;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            restaurantAllQueuePax = itemView.findViewById(R.id.restaurantAllQueuePax);
+            restaurantAllQueueSeatNo = itemView.findViewById(R.id.restaurantAllQueueSeatNo);
+            restaurantAllQueueSeatButton = itemView.findViewById(R.id.restaurantAllQueuesSeatButton);
+            restaurantAllQueueCancelButton = itemView.findViewById(R.id.restaurantAllQueuesCancelButton);
+
+
+        }
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        // Inflate the custom layout
+        View restaurantSeatsView = inflater.inflate(R.layout.row_restaurant_allqueue_recycleview, parent, false);
+
+        // Return a new holder instance
+        ViewHolder restaurantSeatsViewHolder = new ViewHolder(restaurantSeatsView);
+        return restaurantSeatsViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        // Get the data model based on position
+        Seat seat = seats.get(position);
+
+        // Set item views based on your views and data model
+        TextView restaurantAllQueuePax = holder.restaurantAllQueuePax;
+        TextView restaurantAllQueueSeatNo = holder.restaurantAllQueueSeatNo;
+        Button restaurantAllQueueSeatButton = holder.restaurantAllQueueSeatButton;
+        Button restaurantAllQueueCancelButton = holder.restaurantAllQueueCancelButton;
+        restaurantAllQueuePax.setText(seat.getNoOfPax());
+        restaurantAllQueueSeatNo.setText(seat.getSeatNo());
+
+        restaurantAllQueueCancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                seats.remove(position);
+                notifyItemRemoved(position);
+            }
+        });
+
+        restaurantAllQueueSeatButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                int a = holder.getAdapterPosition();
+                Intent intent = new Intent(v.getContext(), RestaurantSeatCustomerActivity.class);
+                Bundle b = new Bundle();
+                b.putSerializable("seat", seat);
+                intent.putExtras(b);
+                v.getContext().startActivity(intent);
+
+//                intent.putExtra("noOfPax", seats.get(position).getNoOfPax());
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return seats.size();
+    }
+}
