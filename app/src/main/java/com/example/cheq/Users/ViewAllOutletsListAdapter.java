d1c29f8567c2dea1940a71d731d83e946b1537aa
Sherene <sherene_tan@mymail.sutd.edu.sg>
2020-11-19 15:11:20 +0800

    update

--- app/src/main/java/com/example/cheq/Users/ViewAllOutletsListAdapter.java
+++ app/src/main/java/com/example/cheq/Users/ViewAllOutletsListAdapter.java
@@ -14,35 +14,37 @@ import com.example.cheq.R;
 import com.example.cheq.Utils.Utils;
 
 import java.io.IOException;
-import java.lang.reflect.Array;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class ViewOutletsListAdapter extends RecyclerView.Adapter<com.example.cheq.Users.ViewOutletsListAdapter.ViewHolder> {
+public class ViewAllOutletsListAdapter extends RecyclerView.Adapter<com.example.cheq.Users.ViewAllOutletsListAdapter.ViewHolder> {
 
     private static final String TAG = "retrieve id";
     ArrayList<String> restaurantNames;
     ArrayList<String> categories;
     ArrayList<String> restaurantImages;
+    ArrayList<String> restaurantWaitingTimes;
     static ArrayList<String> restaurantID;
 
-    private onRestaurantListener mOnRestaurantListener;
+    private ViewAllOutletsListAdapter.onRestaurantListener mOnRestaurantListener;
 
     public static class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
         TextView nameTextView;
         TextView categoryTextView;
         ImageView imageView;
+        TextView waitingTimeTextView;
 
-        onRestaurantListener onRestaurantListener;
+        ViewAllOutletsListAdapter.onRestaurantListener onRestaurantListener;
 
-        public ViewHolder(View itemView, onRestaurantListener listener) {
+        public ViewHolder(View itemView, ViewAllOutletsListAdapter.onRestaurantListener listener) {
             super(itemView);
-            imageView = itemView.findViewById(R.id.restaurant_image);
-            nameTextView = itemView.findViewById(R.id.restaurant_name);
-            categoryTextView = itemView.findViewById(R.id.category);
+            imageView = itemView.findViewById(R.id.restaurantImage);
+            nameTextView = itemView.findViewById(R.id.restaurantName);
+            categoryTextView = itemView.findViewById(R.id.restaurantCategory);
+            waitingTimeTextView = itemView.findViewById(R.id.waitingTime);
             this.onRestaurantListener = listener;
 
             itemView.setOnClickListener(this);
@@ -66,35 +68,42 @@ public class ViewOutletsListAdapter extends RecyclerView.Adapter<com.example.che
         public TextView getNameTextView() {
             return nameTextView;
         }
+
+        public TextView getWaitingTimeTextView() {
+            return waitingTimeTextView;
+        }
     }
 
     // Constructor
-    public ViewOutletsListAdapter(HashMap<String, HashMap<String, String>> info, onRestaurantListener onRestaurantListener) {
+    public ViewAllOutletsListAdapter(HashMap<String, HashMap<String, String>> info, ViewAllOutletsListAdapter.onRestaurantListener onRestaurantListener) {
         this.restaurantNames = new ArrayList<>();
         this.categories = new ArrayList<>();
         this.restaurantImages = new ArrayList<>();
         this.restaurantID = new ArrayList<>();
+        this.restaurantWaitingTimes = new ArrayList<>();
 
         for (String id: info.keySet()) {
             this.restaurantID.add(id);
             this.restaurantNames.add(info.get(id).get("name"));
             this.categories.add(info.get(id).get("category"));
+            this.restaurantWaitingTimes.add(info.get(id).get("waitingTime"));
+            this.restaurantImages.add(info.get(id).get("image"));
         }
         this.mOnRestaurantListener = onRestaurantListener;
     }
 
     // Create new views (invoked by the layout manager)
     @Override
-    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public ViewAllOutletsListAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         // create a new view
         View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.outlets_list_details, parent, false);
 
-        return new ViewHolder(v, mOnRestaurantListener);
+        return new ViewAllOutletsListAdapter.ViewHolder(v, mOnRestaurantListener);
     }
 
     // Replace the contents of a view (invoked by the layout manager)
     @Override
-    public void onBindViewHolder(ViewHolder holder, int position) {
+    public void onBindViewHolder(ViewAllOutletsListAdapter.ViewHolder holder, int position) {
 
         // retrieve the data for that position
         holder.getNameTextView().setText(restaurantNames.get(position));
