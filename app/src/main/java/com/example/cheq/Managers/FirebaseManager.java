e3b56884c156f73ba26bc7a531ba28a5e5f1cddf
Shan Kai <48236492+shankaikai@users.noreply.github.com>
2020-11-20 09:48:47 +0800

    Revert "Revert "Revert "updated progress for restaurant onboarding"""
    This reverts commit a0b9c322d2b88d87003155bee8bba75b3ffc65a6.

--- app/src/main/java/com/example/cheq/Managers/FirebaseManager.java
+++ app/src/main/java/com/example/cheq/Managers/FirebaseManager.java
@@ -3,13 +3,10 @@ package com.example.cheq.Managers;
 
 import com.example.cheq.Entities.RestaurantInfo;
 import com.example.cheq.Entities.User;
-import com.example.cheq.Login.RestaurantOnboard.DishItem;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-import java.util.ArrayList;
-
-// This class handles all firebase insert interactions
+// This class handles all firebase interactions
 public class FirebaseManager {
 
     public FirebaseDatabase firebaseInstance;
@@ -34,14 +31,5 @@ public class FirebaseManager {
 
         // Insert user into firebase
         firebaseReference.child(userPhone).setValue(restaurantInfo);
-    }
-
-    // Upload menu to database
-    private void uploadMenu(String userPhone, ArrayList<DishItem> menuList) {
-        // Set reference to "Users" child
-        DatabaseReference firebaseReference = firebaseInstance.getReference("Menus");
-
-        // Insert user into firebase
-        firebaseReference.child(userPhone).setValue(menuList);
     }
 }
