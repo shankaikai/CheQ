020a681c1aab33dbb989d6d06e2806c789db0b66
Shan Kai <48236492+shankaikai@users.noreply.github.com>
2020-11-29 21:27:54 +0800

    Revert "Revert "Merge branch 'kaye_branch5' into sk""
    This reverts commit cca3d4d73bdf7fd3da021c5dd78b105554921d85.

--- app/src/main/java/com/example/cheq/Users/UserPastActivitiesRecyclerViewAdapter.java
+++ app/src/main/java/com/example/cheq/Users/UserPastActivitiesRecyclerViewAdapter.java
@@ -3,6 +3,7 @@ package com.example.cheq.Users;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
+import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,8 +12,10 @@ import android.widget.TextView;
 
 import com.example.cheq.R;
 
-
+import java.util.ArrayList;
 import java.util.List;
+
+import static java.security.AccessController.getContext;
 
 /**
  * {@link RecyclerView.Adapter} that can display a {@link DummyItem}.
@@ -20,17 +23,18 @@ import java.util.List;
  */
 public class UserPastActivitiesRecyclerViewAdapter extends RecyclerView.Adapter<UserPastActivitiesRecyclerViewAdapter.ViewHolder> {
 
-    String names[], dates[], groupsizes[];
-    int images[];
-//    Context context;
+    ArrayList<String> names, dates, groupsizes;
+    ArrayList<Uri> images;
+    Context context;
 
 
 
-    public UserPastActivitiesRecyclerViewAdapter(String[] names, String[] dates, String[] groupsizes, int[] images) {
+    public UserPastActivitiesRecyclerViewAdapter(ArrayList<String> names, ArrayList<String> dates, ArrayList<String> groupsizes, ArrayList<Uri> images, Context context) {
         this.names = names;
         this.dates = dates;
         this.groupsizes = groupsizes;
         this.images = images;
+        this.context = context;
     }
 
     @Override
@@ -42,15 +46,20 @@ public class UserPastActivitiesRecyclerViewAdapter extends RecyclerView.Adapter<
 
     @Override
     public void onBindViewHolder(final ViewHolder holder, int position) {
-        holder.name.setText(names[position]);
-        holder.date.setText(dates[position]);
-        holder.groupsize.setText(groupsizes[position]);
-        holder.image.setImageResource(images[position]);
+        holder.name.setText(names.get(position));
+        holder.date.setText(dates.get(position));
+        holder.groupsize.setText(groupsizes.get(position));
+        holder.image.setImageURI(images.get(position));
+        Glide.with(this.context).load(images.get(position)).centerCrop().into(holder.getImage());
     }
 
     @Override
     public int getItemCount() {
-        return names.length;
+        if (names == null) {
+            return 0;
+        }
+
+        return names.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
@@ -65,6 +74,10 @@ public class UserPastActivitiesRecyclerViewAdapter extends RecyclerView.Adapter<
             date = (TextView) view.findViewById(R.id.past_activity_date);
             groupsize = (TextView) view.findViewById(R.id.past_activity_groupsize);
             image = view.findViewById(R.id.past_activity_imageView);
+        }
+
+        public ImageView getImage() {
+            return image;
         }
     }
 }
